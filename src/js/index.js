147ec69da1ee5a5fd2e3398807afb10f49d74962
Bence Varga <vbence86@gmail.com>
2016-12-30 17:08:50 +0000

    refactor essentials

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,14 @@
 'use strict';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Button } from 'react-bootstrap';
 
 const root = document.getElementById('root');
 
 class HelloMessage extends React.Component {
-  render() {
-    return <div>Hello {this.props.name}</div>;
-  }
+    render() {
+        return <div><div>Hello {this.props.name}</div><Button bsStyle="primary">Primary</Button></div>
+    }
 }
 
 ReactDOM.render(<HelloMessage name="Juane" />, root);
