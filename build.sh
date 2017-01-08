26e6732fa3b52f25d6a17463db7a9bda7f376f3f
Bence Varga <vbence86@gmail.com>
2017-01-08 23:02:31 +0000

    amend styling for hero-video

--- build.sh
+++ build.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-export NODE_ENV=production
-export CONTENTFUL_SPACE=v9u7v7jawm36
-export CONTENTFUL_ACCESS_TOKEN=a15a60af447bb88662e7eed5dc8ed2042d623307cfbda6b9c1e7aa849ca15ef7
-
 set -e
-
-npm run lint
-webpack --bail
 
 SOURCE=src
 DIST=dist
@@ -16,6 +9,16 @@ DIR_CSS=css
 DIR_FONTS=fonts
 
 RESOURCES=resources
+
+export NODE_ENV=production
+export CONTENTFUL_SPACE=v9u7v7jawm36
+export CONTENTFUL_ACCESS_TOKEN=a15a60af447bb88662e7eed5dc8ed2042d623307cfbda6b9c1e7aa849ca15ef7
+
+rm -R $DIST
+
+
+npm run lint
+webpack --bail
 
 cp -r $SOURCE/$DIR_SASS $DIST/$DIR_CSS
 cp -r $SOURCE/$DIR_FONTS $DIST/$DIR_FONTS
