8a62d0ee652e99d38c3418f65431231c7e1a6a54
Bence Varga <vbence86@gmail.com>
2017-02-01 23:08:55 +0000

    add required class to provide styling to modals

--- src/js/components/ErrorModal.js
+++ src/js/components/ErrorModal.js
@@ -18,7 +18,7 @@ export default class ErrorModal extends React.Component {
   render() {
     const modalId = this.props.modalId;
     return (
-      <div className="modal fade modal-error" key={modalId} id={modalId}
+      <div className="modal fade neal-signup-modal modal-error" key={modalId} id={modalId}
       tabIndex="-1" role="dialog" aria-hidden="true">
         <div className="modal-dialog" role="document">
           <div className="modal-content">
